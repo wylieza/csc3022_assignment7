6d47fcd94eb5c02ec4d9876cc3c61130f7fc1f44
SpectrumX <wylieza@gmail.com>
2020-06-02 17:34:57 +0200

    Initial class structure defined

--- perceptron.hpp
+++ perceptron.hpp
@@ -2,13 +2,26 @@
 #define PERCEPTRON
 
 //Includes
+#include <vector>
 
 namespace WYLJUS002{
 
 class perceptron{
+    public:
+    int idim; //input dimension (no. of incomming xi's)
     double w0; //bias values
-    double w[]; //weights
+    std::vector<double> w; //weights
     double n; //learning rate
+    std::vector<double> x; //inputs
+    int output; //output (active or not)
+
+    perceptron(const int &dimension);
+
+    void train(const int &i, const double &xi, const int &expected);
+
+    int compute();
+
+    int compute(std::vector<double> x_inputs);
 
 
 };
