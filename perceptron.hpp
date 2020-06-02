c2525fd4085d5365a30297f411f76340cbf42b28
SpectrumX <wylieza@gmail.com>
2020-06-02 17:11:24 +0200

    perceptron class source file creation

---
+++ perceptron.hpp
@@ -0,0 +1,19 @@
+#ifndef PERCEPTRON
+#define PERCEPTRON
+
+//Includes
+
+namespace WYLJUS002{
+
+class perceptron{
+    double w0; //bias values
+    double w[]; //weights
+    double n; //learning rate
+
+
+};
+
+}
+
+
+#endif
