b04a17905ac02ddff8601656b937c09462aa55ea
SpectrumX <wylieza@gmail.com>
2020-06-09 22:40:16 +0200

    created function to test perceptron results

--- driver.cpp
+++ driver.cpp
@@ -2,9 +2,21 @@
 
 using namespace WYLJUS002;
 
-void print_compute(std::vector<double> x_inputs, perceptron &pcp){
+void print_compute(std::vector<double> &x_inputs, perceptron &pcp){
     int result = pcp.compute(x_inputs);
     std::cout << "Input of " << x_inputs[0] << ", " << x_inputs[1] << " => " << result << std::endl; 
+}
+
+bool check_and(std::vector<double> &x_inputs, const int &result){
+    return ((int)x_inputs[0] & (int)x_inputs[1]) == result;
+}
+
+bool check_or(std::vector<double> &x_inputs, const int &result){
+    return ((int)x_inputs[0] | (int)x_inputs[1]) == result;
+}
+
+bool check_nand(std::vector<double> &x_inputs, const int &result){
+    return !((int)x_inputs[0] & (int)x_inputs[1]) == result;
 }
 
 int main(int argc , const char** argv){
@@ -17,8 +29,14 @@ int main(int argc , const char** argv){
     perceptron or_perceptron(2);
     perceptron nand_perceptron(2);
 
-    for(int i = 0; i < 4*1000; i++){
-        //print_compute(training_and[i%4][0], and_perceptron);
+    int max_iterations = 10000;
+
+    std::vector<double> x = {0, 1};
+    std::cout << "stefaaann my boi... watch dis space -> " << check_nand(x, 1) << '\n';
+
+    //Train AND Gate
+
+    for(int i = 0; i < max_iterations; i++){
         and_perceptron.train(training_and[i%4][0], (int) training_and[i%4][1][0]);
     }
 
