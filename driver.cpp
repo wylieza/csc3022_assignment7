78c16ac0c5c07c0a1c896480fddbf0c80bf02070
SpectrumX <wylieza@gmail.com>
2020-06-09 20:51:17 +0200

    created object of perceptron class and a training loop for the and ANN

--- driver.cpp
+++ driver.cpp
@@ -2,13 +2,31 @@
 
 using namespace WYLJUS002;
 
+void print_compute(std::vector<double> x_inputs, perceptron &pcp){
+    int result = pcp.compute(x_inputs);
+    std::cout << "Input of " << x_inputs[0] << ", " << x_inputs[1] << " => " << result << std::endl; 
+}
+
 int main(int argc , const char* argv){
-    std::vector<perceptron> input_layer(2);
-    std::vector<perceptron> hidden_layer(2);
-    std::vector<perceptron> output_layer(1);
+    std::vector<std::vector<std::vector<double>>> training_xor = {{{0,0},{0}}, {{0, 1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
+    std::vector<std::vector<std::vector<double>>> training_and = {{{0,0},{0}}, {{0, 1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
+    std::vector<std::vector<std::vector<double>>> training_or = {{{0,0},{0}}, {{0, 1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
+    std::vector<std::vector<std::vector<double>>> training_nand = {{{0,0},{0}}, {{0, 1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
 
-    std::vector<std::vector<std::vector<int>>> training_sets = {{{0,0},{0}}, {{0, 1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
+    perceptron and_perceptron(2);
+    perceptron or_perceptron(2);
+    perceptron nand_perceptron(2);
 
-    
+    for(int i = 0; i < 4; i++){
+        print_compute(training_and[i][0], and_perceptron);
+        and_perceptron.train(training_and[i][0], (int) training_and[i][1][0]);
+    }
+
+    std::cout << "Training complete\n";
+
+    for(int i = 0; i < 4; i++){
+        print_compute(training_and[i][0], and_perceptron);
+    }
+
 
 }
