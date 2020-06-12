bc0b03a7b8bb8ba6675f4e3cd7c98a22d2eb1dc0
SpectrumX <wylieza@gmail.com>
2020-06-12 17:09:10 +0200

    program calculates the final output neurons value (question 4)

--- ann_driver.cpp
+++ ann_driver.cpp
@@ -16,11 +16,21 @@ int main(int argc , const char** argv){
     hidden_1.w0 = 0.1;
     hidden_2.w = {-0.4, 1.0, -0.6};
     hidden_2.w0 = -0.3;
+    output.w = {0.8, 1.0};
+    output.w0 = -0.3;
 
     //Find the hidden layer neuron values
-    std::cout << "Hidden layer neuron values:\n";
+    std::cout << "Question 3) Hidden layer neuron values:\n";
     std::cout << "Hidden Neuron 1 -> " << hidden_1.compute(x_inputs) << '\n';
     std::cout << "Hidden Neuron 2 -> " << hidden_2.compute(x_inputs) << '\n';
+
+
+    //Find the output neuron value
+    std::cout << "Question 4) Output neuron value:\n";
+    std::cout << "Output Neuron -> " << output.compute({hidden_1.compute(x_inputs), hidden_2.compute(x_inputs)}) << '\n';
+
+
+    
 
     
 
